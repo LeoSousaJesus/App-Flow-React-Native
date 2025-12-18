93b9dcba34c3645bacf4aac6ba3f94dc7b0c6381
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-12-18 00:06:27 -0300

    Adicionando Thymeleaf

--- src/main/java/projetofinal/flow/FlowApplication.java
+++ src/main/java/projetofinal/flow/FlowApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlowApplication.class, args);
+
+			SpringApplication.run(FlowApplication.class, args);
 	}
 
 }
